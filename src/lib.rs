a17c18acce95c4890fd878cb8410b602459fcc23
root <138797599+m1c3lium@users.noreply.github.com>
2024-02-02 08:57:10 +0000

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -17,33 +17,120 @@ struct Message {
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[serde(untagged)]
 enum Response {
-    Data {
-        model: String,
-        created_at: String,
-        message: Message,
-        done: bool,
-    },
-
     Done {
         model: String,
         created_at: String,
         eval_count: usize,
         done: bool,
     },
+
+    Data {
+        model: String,
+        created_at: String,
+        message: Message,
+        done: bool,
+    },
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(untagged)]
+enum ResponseRaw {
+    Done {
+        model: String,
+        created_at: chrono::DateTime<chrono::Local>,
+        eval_count: usize,
+        done: bool,
+    },
+
+    Data {
+        model: String,
+        created_at: chrono::DateTime<chrono::Local>,
+        response: String,
+        done: bool,
+    },
 }
 
 #[cfg(test)]
 mod tests {
-    use std::process::Output;
-
     use super::*;
-    use futures_util::{Stream, StreamExt};
+    use futures_util::StreamExt;
+
+    #[tokio::test]
+    async fn test_ollama_chat_endpoint() -> anyhow::Result<()> {
+        let schema = "{\n\t\
+            \"name\": \"add_two_numbers\",\n\t\
+            \"description\": \"Adds two numbers together.\",\n\t\
+            \"signature\": \"(first_number: int, second_number: int) -> int\",\n\t\
+            \"output\": \"<class 'int'>\",\n\
+        }";
+        let query = "Add 2 and 5";
+        let prompt = format!(
+            "\
+            <s> [INST]You are a helpful assistant designed to output JSON.\n\
+            Given the following function schema\n\
+            < {schema} >\n\
+            and query\n\
+            < {query} >\n\
+            extract the parameters values from the query, in a valid JSON format.\n\
+            Example:\n\
+            Input:\n\
+            query: \"How is the weather in Hawaii right now in International units?\"\n\
+            schema:\n\
+            {{\n\t\
+                \"name\": \"get_weather\",\n\t\
+                \"description\": \"Useful to get the weather in a specific location\",\n\t\
+                \"signature\": \"(location: str, degree: str) -> str\",\n\t\
+                \"output\": \"<class 'str'>\",\n\
+            }}\
+            [/INST] \
+            Result: {{\n\t\
+                \"location\": \"London\",\n\t\
+                \"degree\": \"Celsius\",\n\
+            }}</s> \
+            [INST]Input:\n\
+            query: {query}\n\
+            schema: {schema}\
+            [/INST] \
+            Result: \
+            "
+        );
+
+        use colored::Colorize;
+        let p_prompt = format!("TEMPLATE ```\n{prompt}\n```")
+            .bright_yellow()
+            .dimmed();
+        let p_query = format!(">>> {query}").yellow();
+        println!("{p_prompt}\n\n{p_query}",);
+
+        let stream = reqwest::Client::new()
+            .post("http://0.0.0.0:11434/api/generate")
+            .json(&serde_json::json!({
+                "model": "mistral:instruct",
+                "prompt": prompt
+            }))
+            .send()
+            .await?
+            .bytes_stream();
+
+        let mut stream = json_stream::JsonStream::<ResponseRaw, _>::new(stream);
+
+        println!("{}", "\n\n==Response==\n".dimmed());
+        while let Some(Ok(item)) = stream.next().await {
+            match item {
+                ResponseRaw::Done { .. } => (),
+                ResponseRaw::Data { response, .. } => print!("{response}"),
+            }
+            // println!("Chunk: {:?}", item.unwrap());
+        }
+        println!("{}", "\n\n==End of Response==".dimmed());
+
+        Ok(())
+    }
 
     // Implementation of mock
     // ollama client for tests
-
-    #[tokio::test]
-    async fn test_ollama_chat_api() {
+    // #[tokio::test]
+    async fn test_ollama_api() {
         let line = "The quick brown fox jumps over a lazy dog";
         let mut s = line.split_inclusive(' ');
 
@@ -102,3 +189,24 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+// let (B_FUNC, E_FUNC) = ("<FUNCTIONS>", "</FUNCTIONS>");
+// let (B_INST, E_INST) = ("[INST]", "[/INST]");
+// let (B_SYS, E_SYS) = ("<s>", "</s>");
+//
+// let f = serde_json::json!({
+//     "function": "search_bing",
+//     "description": "Search the web for content on Bing. This allows users to search online/the internet/the web for content.",
+//     "arguments": [
+//         {
+//             "name": "query",
+//             "type": "string",
+//             "description": "The search query string"
+//         }
+//     ]
+// });
+//
+// let system_prompt = "You have access to the following functions. Use them if required:";
+// let user_prompt = "Search for the latest news on AI.";
+//
+// let prompt = format!("{B_FUNC}{f}{E_FUNC}\n\n{B_INST} {B_SYS}\n{system_prompt}\n{E_SYS}\n\n{user_prompt} {E_INST}\n\n");
